ed15bd939d4339370fbe174286f8ae4ac17b842b
KaitoTakenaka <kaito.takenaka@fox-hound.jp>
2025-06-11 12:54:20 +0900

    タスク2-3 完了

--- app/users/[id]/edit/page.tsx
+++ app/users/[id]/edit/page.tsx
@@ -4,16 +4,22 @@
 
 import React from "react";
 import EditUserForm from "../../../../components/EditUserForm";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { Typography, Box } from "@mui/material";
 
-// TODO: URLパラメータからユーザーIDを取得し、EditUserFormコンポーネントに渡す
+// EditUserFormコンポーネントに渡す
 const EditUserPage: React.FC = () => {
+  const params = useParams();
+  const router = useRouter();
+  // URL から id を取得し、数値に変換
+  const userId = Number(params.id);
+
   return (
     <Box sx={{ mt: 4 }}>
       <Typography variant="h4" gutterBottom>
         ユーザー編集
       </Typography>
+      <EditUserForm userId={userId} onSuccess={() => router.push("/users")} />
     </Box>
   );
 };
