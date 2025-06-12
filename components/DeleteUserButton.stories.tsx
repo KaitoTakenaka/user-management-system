5dec8ea32cb90eb4e2119a28eed56266fee0b642
KaitoTakenaka <kaito.takenaka@fox-hound.jp>
2025-06-12 14:38:02 +0900

    day2_タスク2-2 修正
    言語化で使用していた番号（2.メタデータの定義）などが残ったままだったので修正

--- components/DeleteUserButton.stories.tsx
+++ components/DeleteUserButton.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import DeleteUserButton from "./DeleteUserButton";
 
-//2.メタデータの定義
+//メタデータの定義
 const meta: Meta<typeof DeleteUserButton> = {
   title: "Components/DeleteUserButton", // Storybookのサイドバーに表示される名前
   component: DeleteUserButton, // このストーリーで使うコンポーネント
@@ -9,10 +9,10 @@ const meta: Meta<typeof DeleteUserButton> = {
 
 export default meta;
 
-//3.ストーリーの定義
+//ストーリーの定義
 type Story = StoryObj<typeof DeleteUserButton>;
 
-//4.デフォルトストーリーに例となるユーザーIDを設定
+//デフォルトストーリーに例となるユーザーIDを設定
 export const Default: Story = {
   args: {
     userId: 1,
