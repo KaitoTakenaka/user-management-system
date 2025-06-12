6fa661fc1f4b0755ade9614921a38b4a4f554f15
KaitoTakenaka <kaito.takenaka@fox-hound.jp>
2025-06-12 15:28:39 +0900

    day2_タスク2-3 完了

--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import { User } from '../types/User';//User.tsからUser型をimport
-import UserCard from './UserCard';
-import { Box } from '@mui/material';
+import React ,{ useState }from "react";
+import { User } from "../types/User"; //User.tsからUser型をimport
+import UserCard from "./UserCard";
+import { Box } from "@mui/material";
 
-// 2.props の型を定義(users: User[] 型の props を受け取る)
+// props の型を定義(users: User[] 型の props を受け取る)
 export interface UserListProps {
   users: User[];
 }
 
-// 3.関数コンポーネントの定義
+// 関数コンポーネントの定義
 const UserList: React.FC<UserListProps> = ({ users }) => {
-   return (
-    //4.受け取ったusersをmap処理でループし、UserCardを呼び出す
+  const [userList, setUserList] = useState<User[]>(users);//2-3-a:useStateでusersを保持
+
+  //  2-3-4-a.filter処理で削除されたユーザーを除外して再描画
+  const handleDelete = (deletedUserId: number) => {
+    setUserList((prevUsers) =>
+      prevUsers.filter((user) => user.id !== deletedUserId)
+    );
+  };
+  return (
+    //2-3-4-b.受け取ったusersをmap処理でループし、UserCardを呼び出す
     <Box>
-      {users.map((user) => (
-        <UserCard key={user.id} user={user} />
+      {userList.map((user) => (
+        <UserCard key={user.id} user={user} onDelete={handleDelete}/>
       ))}
     </Box>
   );
