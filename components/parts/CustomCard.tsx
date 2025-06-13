3f639dbc0f1e50663d61ad1c27574e47c18614b4
KaitoTakenaka <kaito.takenaka@fox-hound.jp>
2025-06-13 17:10:31 +0900

    day3_2-1 完了

--- components/parts/CustomCard.tsx
+++ components/parts/CustomCard.tsx
@@ -3,8 +3,10 @@
 import React from "react";
 import { Card, CardContent, Typography, CardActions } from "@mui/material";
 
-// TODO: インターフェースを修正
+// インターフェースにtitleとdescriptionを追加
 interface CustomCardProps {
+  title: string;
+  description: string;
   actions?: React.ReactNode;
 }
 
@@ -16,9 +18,13 @@ const CustomCard: React.FC<CustomCardProps> = ({
   return (
     <Card sx={{ minWidth: 275, mb: 2 }}>
       <CardContent>
-        {/*
-		      TODO: [titel]と[description]を表示
-		     */}
+        {/*titleとdescriptionを表示*/}
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
       </CardContent>
       {actions && <CardActions>{actions}</CardActions>}
     </Card>
